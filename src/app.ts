9850644d9c6098a4ca4535c7208624909da21da1
Thiago <thiago.goncalvesgames@gmail.com>
2026-06-14 20:55:32 -0300

    docs: adiciona configuração de documentação da API com swagger

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,16 @@ import cors from 'cors';
 import { configDotenv } from 'dotenv';
 configDotenv();
 import router from './routes/router.routes';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './swagger.json';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Rota de documentação do Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/", router);
 
